58735b0f8a0023ad4c2853a5cc05a62094001b35
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-20 17:32:03 +0000

    go generate results

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,5 +1,5 @@
 provider "godaddy-dns" {
-  api_key = "better set it in GODADDY_API_KEY"
+  api_key    = "better set it in GODADDY_API_KEY"
   api_secret = "better set it in GODADDY_API_SECRET"
 }
 
