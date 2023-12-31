371142cfbec94b82715ccde9f3b5fe167044b95d
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-31 19:42:07 +0000

    examples re-organization

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -3,3 +3,10 @@ provider "godaddy-dns" {
   api_secret = "better set it in GODADDY_API_SECRET"
 }
 
+# create "alias.test.com" as as CNAME for "other.com"
+resource "godaddy-dns_record" "my-cname" {
+  domain = "test.com"
+  type   = "CNAME"
+  name   = "alias"
+  data   = "other.com"
+}
--- examples/resources/godaddy-dns_record/resource.tf
+++ examples/resources/godaddy-dns_record/resource.tf
@@ -1,7 +1,39 @@
-resource "godaddy-dns_record" "cname" {
+# one record
+resource "godaddy-dns_record" "one-cname" {
   domain = "mydomain.com"
+  type   = "CNAME"
+  name   = "redirect"
+  data   = "target.otherdomain.com"
+}
 
-  type = "CNAME"
-  name = "redirect"
-  data = "target.otherdomain.com"
+locals {
+  records = {
+    "mx1" = {
+      type = "MX",
+      name = "@",
+      data = "mx01.mail.icloud.com",
+      prio = 10,
+    },
+    "mx2" = {
+      type = "MX",
+      name = "@",
+      data = "mx02.mail.icloud.com",
+      prio = 10,
+    },
+    "spf" = {
+      type = "TXT",
+      name = "@",
+      data = "\"v=spf1 include:icloud.com ~all\""
+    },
+  }
+}
+
+# with names like `godaddy-dns_record.records["mx"]`
+resource "godaddy-dns_record" "records" {
+  for_each = local.records
+  domain   = "mydomain.com"
+  type     = each.value.type
+  name     = each.value.name
+  data     = each.value.data
+  priority = lookup(each.value, "prio", null)
 }
