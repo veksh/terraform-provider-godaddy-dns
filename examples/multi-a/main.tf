aba117e6cbe34f0b15579f68e6c9742a99732f2d
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-02-06 12:35:52 +0000

    vars in multi-a example

--- examples/multi-a/main.tf
+++ examples/multi-a/main.tf
@@ -9,21 +9,21 @@ terraform {
 provider "godaddy-dns" {}
 
 locals {
-    domain     = "veksh.in"
-    subDomain  = "man-test"
-    recName    = "multi-a"
-	  dataValues = ["3.3.3.3", "4.4.4.4"]
+  domain     = "veksh.in"
+  subDomain  = "man-test"
+  recName    = "multi-a"
+  dataValues = ["3.3.3.3", "4.4.4.4"]
 }
 
 # terraform import 'godaddy-dns_record.as[0]' veksh.in:A:multi-a.man-test:1.1.1.1
 resource "godaddy-dns_record" "as" {
-  domain = "${local.domain}"
+  domain = local.domain
   type   = "A"
   name   = "${local.recName}.${local.subDomain}"
 
   # add is always ok, mod/del could be problematic
   # conservative: terraform plan -destroy -parallelism=1
-  count  = length(local.dataValues)
-  data   = local.dataValues[count.index]
-  ttl    = 600
+  count = length(local.dataValues)
+  data  = local.dataValues[count.index]
+  ttl   = 600
 }
