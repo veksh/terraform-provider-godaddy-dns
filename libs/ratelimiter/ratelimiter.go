b2ac6f5297fa1555f5e504e18a965fd8b09eb67b
Adil Bukhari <adil@joinpromise.com>
2024-02-16 11:45:59 -0500

    rework rate limiter to work consistently with GoDaddy API limit of 60 requests per minute
    rate limiter: slight adjustment to work off first token use time instead of last token use time
    
    fix parenthesis placement
    
    correct time calculation
    
    variable naming
    
    slight tweak to prevent negative values

--- libs/ratelimiter/ratelimiter.go
+++ libs/ratelimiter/ratelimiter.go
@@ -7,6 +7,12 @@ import (
 	"time"
 )
 
+const (
+	// number of seconds required to refresh all tokens
+	TOKEN_REFRESH_TIME = 60
+	TOKEN_REFRESH_AS_SECONDS = time.Second * TOKEN_REFRESH_TIME
+)
+
 // simple token bucket rate limiter
 // - keeps track of available tokens and the last time a token was added
 // - on acquire: if there are tokens available, spend one and return immediately
@@ -16,14 +22,12 @@ import (
 //   - if none, wait until next one will be available (while holding mutex)
 type RateLimiter struct {
 	mu sync.Mutex
-	// interval between tokens, s
-	period time.Duration
 	// total bucket size; 1 means "no bursts"
 	bucketSize int
 	// current number of tokens in bucket
 	numTokens int
-	// the time last token was added
-	lastTokenTime time.Time
+	// the time the first token was used in this bucket
+	firstTokenUsedTime time.Time
 }
 
 // context to cancel, rate per second, burst (bucket) size
@@ -32,10 +36,9 @@ func New(rate, burst int) (*RateLimiter, error) {
 		return nil, errors.New("limiter rate and burst must be positive")
 	}
 	return &RateLimiter{
-		period:        time.Second / time.Duration(rate),
 		bucketSize:    burst,
 		numTokens:     burst,
-		lastTokenTime: time.Now(),
+		firstTokenUsedTime: time.Now(),
 	}, nil
 }
 
@@ -43,27 +46,34 @@ func New(rate, burst int) (*RateLimiter, error) {
 func (s *RateLimiter) Wait() {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	// if bucket is empty: add tokens that are due, update last refill time
+
+	// if bucket is empty: wait for TOKEN_REFRESH_TIME seconds since the first token was used, refill the bucket to (bucketSize),
+	// and set the first token time to time.Now() to set the new start of the period.
+	// this will help us comply with the GoDaddy APIs, which limit us to 60 requests every minute per endpoint.
+	// See: https://developer.godaddy.com/getstarted#terms
 	if s.numTokens <= 0 {
-		elapsedTime := time.Since(s.lastTokenTime)
-		tokensToAdd := int(elapsedTime.Nanoseconds() / s.period.Nanoseconds())
-		s.numTokens = tokensToAdd
-		if s.numTokens > s.bucketSize {
-			s.numTokens = s.bucketSize
+
+		// if less than TOKEN_REFRESH_TIME seconds has passed since firstTokenUsedTime,
+		// calculate firstTokenUsedTime + TOKEN_REFRESH_TIME and sleep until that time
+		if (time.Since(s.firstTokenUsedTime) <= TOKEN_REFRESH_AS_SECONDS) {
+			minimumRefreshTime := s.firstTokenUsedTime.Add(TOKEN_REFRESH_AS_SECONDS)
+			requiredTokenRefreshDuration := time.Since(minimumRefreshTime).Abs()
+
+			if (requiredTokenRefreshDuration > TOKEN_REFRESH_AS_SECONDS) {
+				// ensure we only sleep for TOKEN_REFRESH_AS_SECONDS seconds maximum
+				requiredTokenRefreshDuration = TOKEN_REFRESH_AS_SECONDS
+			}
+			time.Sleep(requiredTokenRefreshDuration)
 		}
-		s.lastTokenTime = s.lastTokenTime.Add(time.Duration(tokensToAdd) * s.period)
-	}
-	// if token available: ok, take it and release caller from the wait
-	if s.numTokens > 0 {
+
+		// now reset tokens and set first token use time
+		s.numTokens = s.bucketSize
+		s.firstTokenUsedTime = time.Now()
+	} else {
+		// if token available: ok, take it and release caller from the wait
 		s.numTokens--
 		return
 	}
-	// if still no tokens: will have to wait until nextTokenTime or cancel
-	nextTokenTime := s.lastTokenTime.Add(s.period)
-	waitDuration := time.Until(nextTokenTime)
-	time.Sleep(waitDuration)
-	// upd last token time and release from the wait
-	s.lastTokenTime = nextTokenTime
 }
 
 // block until token becomes available, with cancellable context
@@ -73,30 +83,45 @@ func (s *RateLimiter) WaitCtx(ctx context.Context) error {
 	}
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	// if bucket is empty: add tokens that are due, update last refill time
+	// if bucket is empty: wait for TOKEN_REFRESH_AS_SECONDS seconds since the last token was used, refill the bucket to (bucketSize),
+	// and set the last token time to time.Now() to set the new start of the period.
+	// this is unique to the GoDaddy APIs, which limit us to 60 requests every minute per endpoint.
+	// See: https://developer.godaddy.com/getstarted#terms
 	if s.numTokens <= 0 {
-		elapsedTime := time.Since(s.lastTokenTime)
-		tokensToAdd := int(elapsedTime.Nanoseconds() / s.period.Nanoseconds())
-		s.numTokens = tokensToAdd
-		if s.numTokens > s.bucketSize {
-			s.numTokens = s.bucketSize
+		// if less than TOKEN_REFRESH_AS_SECONDS seconds has passed since firstTokenUsedTime,
+		// calculate firstTokenUsedTime + TOKEN_REFRESH_AS_SECONDS and sleep until that time
+		requiredTokenRefreshDuration := time.Second * 0
+		if (time.Since(s.firstTokenUsedTime) <= TOKEN_REFRESH_AS_SECONDS) {
+			minimumRefreshTime := s.firstTokenUsedTime.Add(TOKEN_REFRESH_AS_SECONDS)
+			requiredTokenRefreshDuration = time.Since(minimumRefreshTime).Abs()
+
+			if (requiredTokenRefreshDuration > TOKEN_REFRESH_AS_SECONDS) {
+				// ensure we only sleep for TOKEN_REFRESH_AS_SECONDS seconds maximum
+				requiredTokenRefreshDuration = TOKEN_REFRESH_AS_SECONDS
+			}
+
+			if (requiredTokenRefreshDuration < 0) {
+				// don't wait for a negative amount of time. This is necessary since time.After() doesn't instantly
+				// return on a negative duration.
+				requiredTokenRefreshDuration = time.Second * 0
+			}
 		}
-		s.lastTokenTime = s.lastTokenTime.Add(time.Duration(tokensToAdd) * s.period)
+
+		s.numTokens = s.bucketSize
+
+		select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case <-time.After(requiredTokenRefreshDuration):
+		}
+		// if not cancelled: upd last token time and release from the wait
+		s.firstTokenUsedTime = time.Now()
+		return nil
 	}
 	// if token available: ok, take it and release caller from the wait
 	if s.numTokens > 0 {
 		s.numTokens--
 		return nil
 	}
-	// if still no tokens: will have to wait until nextTokenTime or cancel
-	nextTokenTime := s.lastTokenTime.Add(s.period)
-	waitDuration := time.Until(nextTokenTime)
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-time.After(waitDuration):
-	}
-	// if not cancelled: upd last token time and release from the wait
-	s.lastTokenTime = nextTokenTime
 	return nil
 }
